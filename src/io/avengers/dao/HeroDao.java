66ac2b1bc8821cedb883043a0f204702dbd75b10
raqueldj <raquel.diasjales@gmail.com>
2017-02-24 11:25:51 +0100

    find by name

--- src/io/avengers/dao/HeroDao.java
+++ src/io/avengers/dao/HeroDao.java
@@ -12,34 +12,87 @@ import io.avengers.domain.Hero;
 import io.avengers.domain.Sex;
 
 public class HeroDao {
-	//DAO: direct access object
-	
-	public Set<Hero> findAll() throws ClassNotFoundException, SQLException{
+	// DAO: direct access object
+
+	static Class c;
+
+	public HeroDao() {
+		if (c == null) {
+			try {
+				c = Class.forName("com.mysql.jdbc.Driver");
+			} catch (ClassNotFoundException e) {
+				throw new IllegalStateException("SQL Driver is not here: " + e.getMessage());
+			}
+		}
+	}
+
+	public Set<Hero> findAll() throws SQLException {
 		String query = "SELECT * FROM heroes";
-		
-		Class.forName("com.mysql.jdbc.Driver");
-		Connection connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/marvel", "root", "");
-		
+
+		Connection connect = connectToMySql();
+
 		Statement statement = connect.createStatement();
 		ResultSet resultSet = statement.executeQuery(query);
-		
+
 		Set<Hero> heroes = new HashSet<>();
-		
-		while (resultSet.next()){
+
+		while (resultSet.next()) {
+
+			heroes.add(resultSetToHero(resultSet));
+		}
+
+		connect.close();
+
+		return heroes;
+	}
+
+	public Set<Hero> findHeroesByName(String term) throws SQLException {
+		String query = "SELECT h.name FROM heroes h WHERE name LIKE%" + term + "% ORDER BY h.name";
+
+		Connection connect = connectToMySql();
+
+		Statement statement = connect.createStatement();
+		ResultSet resultSet = statement.executeQuery(query);
+
+		Set<Hero> heroes = new HashSet<>();
+
+		while (resultSet.next()) {
+
+			heroes.add(resultSetToHero(resultSet));
+		}
+
+		connect.close();
+
+		return heroes;
+	}
+
+	Hero resultSetToHero(ResultSet resultSet) {
+		try {
 			int id = resultSet.getInt("id");
 			String name = resultSet.getString("name");
-			//pour differencier avec l'enum
+			// pour differencier avec l'enum
 			String sSex = resultSet.getString("sex");
 			long likes = resultSet.getLong("likes");
 			long dislikes = resultSet.getLong("dislikes");
-			
+
 			Hero h = new Hero(id, name, Sex.O, likes, dislikes);
-			heroes.add(h);
+
+			return h;
+		} catch (SQLException e) {
+			throw new IllegalStateException("Database moved: " + e.getMessage());
 		}
-		
-		connect.close();
-		
-		return heroes;
+
 	}
-	
+
+	Connection connectToMySql() {
+		Connection connect;
+		try {
+			connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/marvel", "root", "");
+			return connect;
+		} catch (SQLException e) {
+			throw new IllegalStateException("Wrong credentials, URL or overloaded connection: " + e.getMessage());
+		}
+
+	}
+
 }
--- src/io/avengers/service/HeroService.java
+++ src/io/avengers/service/HeroService.java
@@ -1,14 +1,41 @@
 package io.avengers.service;
 
+import java.sql.SQLException;
 import java.util.Set;
 
 import io.avengers.dao.HeroDao;
 import io.avengers.domain.Hero;
 
 public class HeroService {
+	IllegalStateException stateException = new IllegalStateException("Connection impossible, try again later");
+	
+	public Set<Hero> findAll() {
 
-	public Set<Hero> findAll() throws Exception {
-		return new HeroDao().findAll();
+		try {
+			return new HeroDao().findAll();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw stateException;
+		}
 	}
 
+	public Set<Hero> findHeroesByName(String term) {
+		
+		if(term== null){
+			System.out.println("Potential bug or illegal request");
+			return this.findAll();
+		}
+		
+		if(term.isEmpty()){
+			return this.findAll();
+		}
+		
+		try {
+			return new HeroDao().findHeroesByName(term);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw stateException;
+		}
+
+	}
 }
